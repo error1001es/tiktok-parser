d727470690b0c1e64624a1b74f13dd4c4a7e921f
= <error1001es@gmail.com>
2021-07-05 03:15:51 +0600

    fix

---
+++ parser.js
@@ -0,0 +1,81 @@
+require('dotenv').config()
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+function average(nums) {
+    return nums.reduce((a, b) => (a + b)) / nums.length;
+}
+
+const TikTokScraper = require('tiktok-scraper')
+
+const { GoogleSpreadsheet } = require('google-spreadsheet')
+const credentials = require('./credentials') // the file saved above
+
+async function initialize() {
+    // Initialize the sheet - doc ID is the long id in the sheets URL
+    const doc = new GoogleSpreadsheet(process.env.GOOGLE_TABLE_ID)
+
+    // Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
+    await doc.useServiceAccountAuth(credentials)
+
+    await doc.loadInfo() // loads document properties and worksheets
+    console.log(doc.title)
+
+    const sheet = doc.sheetsByIndex[0]
+
+    const rows = await sheet.getRows()
+
+    for (row of rows){
+        try {
+            const posts = await TikTokScraper.user(row['Название \nаккаунта'], {
+                number: 12,
+                sessionList: [process.env.SID_TT]
+            })
+
+            if(posts.collector[0]){
+                row['Ссылка на \nаккаунт \nTik-Tok'] = `https://www.tiktok.com/@${row['Название \nаккаунта']}`
+                row['Кол-во\nподписчиков\nTik-Tok'] = posts.collector[0].authorMeta.fans
+
+                let videos = posts.collector
+
+                let playCounts = []
+                let shareCounts = []
+                let diggCounts = []
+
+                for(let i=2;i<12;i++){
+                    playCounts.push(videos[i].playCount)
+                    shareCounts.push(videos[i].shareCount)
+                    diggCounts.push(videos[i].diggCount)
+                }
+
+                row['Изменение просмотры'] = average(playCounts) - row['Средние просмотры']
+                row['Средние просмотры'] = average(playCounts)
+
+                row['Изменение лайки'] = average(diggCounts) - row['Средние лайки']
+                row['Средние лайки'] = average(diggCounts)
+
+                row['Изменение репосты'] = average(shareCounts) - row['Средние репосты']
+                row['Средние репосты'] = average(shareCounts)
+            }else{
+                row['Ссылка на \nаккаунт \nTik-Tok'] = 'Аккаунт не найден'
+                row['Средние просмотры'] = '-'
+                row['Средние лайки'] = '-'
+                row['Средние репосты'] = '-'
+            }
+
+            let date = new Date(Date.now())
+            row['Дата обновления'] = date.toLocaleString('ru-RU')
+
+            row.save()
+
+        } catch (error) {
+            console.log(error)
+        }
+        
+        sleep(2000)
+    }
+}
+
+initialize()
